07036008866ae65f5d27cd3d2002eda48a6f55fe
piyush-ally9 <piyushjoshi06@outlook.com>
2024-09-18 14:10:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle light/dark mode
+const themeButton = document.getElementById('themeButton');
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    themeButton.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
